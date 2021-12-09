71014c9abb3dafde36eefc41f337295b38930813
RISHABRAVI23 <81304421+RISHABRAVI23@users.noreply.github.com>
2021-12-09 18:52:04 +0530

    Displayed users in /users

--- recipe-book/src/Components/Users/User/User.js
+++ recipe-book/src/Components/Users/User/User.js
@@ -3,15 +3,18 @@ import { Link } from "react-router-dom";
 
 export default function User(props) {
 	console.log("User.js component.");
+	console.log(props.username);
 	return (
-		<div>
-			<div className="card">
-				<Link to="/">
-					<div className="card-body">
-						Use this kinda element to show user: {props}
-					</div>
-				</Link>
-			</div>
+		<div className="card my-3">
+			<Link
+				to="/"
+				style={{
+					textDecoration: "none",
+				}}>
+				<div className="card-body text-dark">
+					Use this kinda element to show user: {props.username}
+				</div>
+			</Link>
 		</div>
 	);
 }
--- recipe-book/src/Components/Users/Users.js
+++ recipe-book/src/Components/Users/Users.js
@@ -4,40 +4,20 @@ import User from "./User/User";
 
 export default function Users() {
 	const [users, setUsers] = useState([]);
-	const [error, setError] = useState(false);
-	document.querySelector("title").innerHTML = "Users | Recipe Book";
 	useEffect(() => {
 		axios
-			.get("http://localhost:8000/users/")
-			.then((response) => {
-				setUsers(response.data);
+			.get("http://localhost:8000/users")
+			.then((res) => {
+				console.log(res.data);
+				setUsers(res.data);
 			})
-			.catch((error) => {
-				setError(error);
-			});
+			.catch((err) => console.log(err));
 	}, []);
 	return (
-		<div className="container my-3">
-			{error ? (
-				<div
-					className="alert alert-danger alert-dismissible fade show"
-					role="alert">
-					<strong>There has been an error!!</strong> Please check if
-					your credentials are correct. If so, then we might be
-					experiencing some internal server problems or some server
-					downtime. Sorry for the inconvenience. :(
-					<button
-						type="button"
-						className="btn-close"
-						data-bs-dismiss="alert"
-						aria-label="Close"></button>
-				</div>
-			) : (
-				""
-			)}
-			{users.map((u, i) => {
-				console.log(u);
-				return <User />;
+		<div className="container">
+			{users.map((user) => {
+				console.log(user);
+				return <User key={user.id} username={user.username} />;
 			})}
 		</div>
 	);
